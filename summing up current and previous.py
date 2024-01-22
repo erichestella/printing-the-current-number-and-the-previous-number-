a0244e21b77640f353dfe2950a10c7d4ec944cae
erichestella <erichestella05@gmail.com>
2024-01-22 17:15:52 +0800

    adding some list of previous and current number

--- summing up current and previous.py
+++ summing up current and previous.py
@@ -4,3 +4,26 @@ print('\nPrinting current and previous in a range (10)\n')
 #making a list of number
 current_number= list()
 previous_number= list()
+
+#list of current number
+current_number.append('0')
+current_number.append('1')
+current_number.append('2')
+current_number.append('3')
+current_number.append('4')
+current_number.append('5')
+current_number.append('6')
+current_number.append('7')
+current_number.append('8')
+
+#list of previous number
+previous_number.append('0')
+previous_number.append('0')
+previous_number.append('1')
+previous_number.append('2')
+previous_number.append('3')
+previous_number.append('4')
+previous_number.append('5')
+previous_number.append('6')
+previous_number.append('7')
+previous_number.append('8')
