75da28a1c15c228176bdd8539c6a16f4aaaf56eb
erichestella <erichestella05@gmail.com>
2024-01-22 17:19:24 +0800

    putting some controller to add up the previous and current number

--- summing up current and previous.py
+++ summing up current and previous.py
@@ -15,6 +15,7 @@ current_number.append('5')
 current_number.append('6')
 current_number.append('7')
 current_number.append('8')
+current_number.append('9')
 
 #list of previous number
 previous_number.append('0')
@@ -27,3 +28,9 @@ previous_number.append('5')
 previous_number.append('6')
 previous_number.append('7')
 previous_number.append('8')
+
+#it controls the current and previous number to add up 
+for current, prev, in zip(previous_number, current_number): 
+     total_sum = int(current) + int(prev)
+     print('The current number:', prev, 'The previous number: ', current, 'Sum: ', total_sum)
+
